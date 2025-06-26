8031c6b270e7978165e0721a456f0eb6d9ad3444
ManuelSciuto <manuel393.sciuto@gmail.com>
2025-06-26 15:56:43 +0200

    aaa 26/6

--- src/pages/skills/Languages.tsx
+++ src/pages/skills/Languages.tsx
@@ -1,7 +1,7 @@
 function SkillsLanguagesPage() {
   return (
     <div className="flex flex-col gap-y-6 px-6 pt-8">
-      <div className="flex">
+      <div className="flex justify-center">
         <p>Includi i framework</p>
       </div>
       <div className="mx-auto grid w-1/2 gap-4 lg:grid-cols-2">
